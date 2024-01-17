63439347ec3373c284efb8c11cd1f1812d65cb31
ngawinek <natalkagaw@wp.pl>
2024-01-17 20:04:50 +0100

    Dodaj kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,18 @@
+ef dodawanie(x, y):
+return x+ y
+def odejmowanie(x, y)
+return x- y
+def mnozenie(x, y)
+return x * y 
+def dzielenie(x, y) 
+return x / y 
+else: 
+return "Nie mozna dzielic przez zero!" 
+ 
+liczba1 = float(input("Podaj 1 liczbe: ")) 
+liczba2 = float(input("Podaj 2 liczbe: ")) 
+print("Wynik: ") 
+print("Dodawanie: ", dodawanie(liczba1, liczba2)) 
+print("Odejmowanie: ", odejmowanie(liczba1, liczba2)) 
+print("Mnozenie: ", mnozenie(liczba1, liczba2)) 
+print("Dzielenie: ", dzielenie(liczba1, liczba2))
